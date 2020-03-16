ed4d761209d8468b333a695a37be6046bada34d5
Johan Öhlin <johanohlin@gmx.com>
2020-03-16 16:42:58 +0100

    fix according to shellcheck

--- sh/lad.sh
+++ sh/lad.sh
@@ -6,11 +6,11 @@ URL=$1
 
 # echo $@ > ${HOME}/INNEHÅLL && beep -l 75 -r 2 || beep -f 50 -l 250
 
-for URL in $@; do
+for URL in "$@"; do
    if [[ "${URL}" =~ http(s)?://(www\.)?(aftonbladet|comedycentral|di|dn|dplay|efn|expressen|kanal9play|tv4|svd|nickelodeon|ur|viafree|oppetarkiv|tv10play|tv3play|tv4play|tv6play|tv8play|urplay|svtplay)\.se ]]; then
-       $LADDANED $URL
+       $LADDANED "$URL"
    else
-       echo $URL >> $FIL
+       echo "$URL" >> "$FIL"
    fi
 done
 
