72f8c6ae98fa8143063c0ad8a26fcdd0da7d0ef8
Johan Öhlin <johanohlin@gmx.com>
2020-03-16 16:33:03 +0100

    fix according to shellcheck

--- sh/fixa_låtarna.sh
+++ sh/fixa_låtarna.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-cd /media/musik/
+cd /media/musik/ || exit 1
 
 for i in {9..2}; do
-    find . -name "* ($i).ogg" -type f | \
-        grep -v ^./.osorterat | \
-    while read old; do
-        TO=$(($i-1))
-        for j in $(seq 1 $TO); do
-            new="${old% ($i).ogg} ($j).ogg"
-            if ! [ -f "$new" ]; then
-                INSERT="${new#./}"
-                echo $INSERT
-                mv -nv "$old" "$new"
-                mpc -qw update
-                mpc add "$INSERT"
-            fi
-        done
-    done
+        find . -name "* ($i).ogg" -type f | \
+                grep -v ^./.osorterat | \
+                while read -r old; do
+                        TO=$((i-1))
+                        for j in $(seq 1 $TO); do
+                                new="${old% ($i).ogg} ($j).ogg"
+                                if ! [ -f "$new" ]; then
+                                        INSERT="${new#./}"
+                                        echo "$INSERT"
+                                        mv -nv "$old" "$new"
+                                        mpc -qw update
+                                        mpc add "$INSERT"
+                                fi
+                        done
+                done
 
-    find . -name "* (1).ogg" -type f | \
-            grep -v ^./.osorterat | \
-    while read old; do
-        new="${old%' (1).ogg'}.ogg"
+                find . -name "* (1).ogg" -type f | \
+                        grep -v ^./.osorterat | \
+                        while read -r old; do
+                                new="${old%' (1).ogg'}.ogg"
 
-        if ! [ -f "$new" ]; then
-            INSERT="${new#./}"
-            echo insert $INSERT
-            mv -nv "$old" "$new"
-            mpc -qw update
-            mpc add "$INSERT"
-        fi
-    done
-done
-
-exit 0
+                                if ! [ -f "$new" ]; then
+                                        INSERT="${new#./}"
+                                        echo insert "$INSERT"
+                                        mv -nv "$old" "$new"
+                                        mpc -qw update
+                                        mpc add "$INSERT"
+                                fi
+                        done
+                done
--- sh/ladda_ned.sh
+++ sh/ladda_ned.sh
@@ -3,9 +3,8 @@
 DIR="${HOME}/src/ln"
 RET=0
 
-for URL in $@ ; do
+for URL in "$@"; do
         PRET=$RET
-        PRE=
 
         if [[ "${URL}" =~ http(s)?://(www\.|m\.)?youtube\.com/watch\? ]]                 ||
                 [[ "${URL}" =~ http(s)?://(www\.|m\.)?youtube(-nocookie)?\.com/embed(ed)?/ ]] ||
@@ -13,98 +12,87 @@ for URL in $@ ; do
                 [[ "${URL}" =~ http(s)?://invidio\.us/watch\? ]]                          ; then
 
         #torsocks ${DIR}/youtube.sh "${URL}"   || \
-        ${DIR}/youtube.sh "${URL}"          || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
 
-        PRE="YT"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 
 elif [[ "${URL}" =~ http(s)?://((www|player)\.)?vimeo.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET))
 
-        PRE="VI"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http(s)?://(www\.)?metacafe.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
-        PRE="MC"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http(s)?://(www\.)?cjube.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
 
-        PRE="CJ"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http(s)?://(www\.)?dailymotion.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
 
-        PRE="DM"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http://(www\.)?liveleak.com/view\?i= ]]; then
 
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
 
-        PRE="LL"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http(s)?://((w|www)\.)?[A-Za-z0-9]*\.bandcamp.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh -x --audio-format=vorbis "${URL}"   || \
-                ${DIR}/youtube.sh -x --audio-format=vorbis "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh -x --audio-format=vorbis "${URL}"   || \
+                "${DIR}"/youtube.sh -x --audio-format=vorbis "${URL}"    || \
                 RET=$((RET+1))
 
-        PRE="BC"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
 elif [[ "${URL}" =~ http(s)?://((w|www)\.)?soundcloud.com/ ]]; then
 
-        torsocks ${DIR}/youtube.sh --audio-format=vorbis "${URL}"   || \
-                ${DIR}/youtube.sh --audio-format=vorbis "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh --audio-format=vorbis "${URL}"   || \
+                "${DIR}"/youtube.sh --audio-format=vorbis "${URL}"    || \
                 RET=$((RET+1))
 
-        PRE="SC"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
-elif [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/podrss/[0-9]+(\.|\-)[mM][pP]\3 ]]; then
-        ${DIR}/sr.sh "${URL}"     || \
-                RET=$((RET+))
+elif [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/podrss/[0-9]+(\.|\-)[mM][pP]3 ]]; then
+        "${DIR}"/sr.sh "${URL}"     || \
+                RET=$((RET+1))
 
-        PRE="SvR"
         TXT=${URL}
 
 elif [[ "${URL}" =~ http(s)?://(www\.)?(aftonbladet|comedycentral|di|dn|dplay|efn|expressen|kanal9play|tv4|svd|nickelodeon|ur|oppetarkiv|tv10play|tv3play|tv4play|tv6play|tv8play|urplay|svtplay)\.se ]]; then
 
         export DISPLAY=":0"
-        urxvt -title 'svtget' -cd "$(pwd)" -e ${HOME}/src/minsvtget.sh ${URL} &
+        urxvt -title 'svtget' -cd "$(pwd)" -e "${HOME}"/src/minsvtget.sh "${URL}" &
         disown
 
 
         TXT="Öppnades i egent fönster: $?"
-        PRE="STV"
         sleep 1s
 
-elif [[ "${URL}" =~ (\.)[mM][pP]\3 ]]; then
-        ${DIR}/sr.sh "${URL}"     || \
+elif [[ "${URL}" =~ (\.)[mM][pP]3 ]]; then
+        "${DIR}"/sr.sh "${URL}"     || \
                 RET=$((RET+1))
 
-        PRE="MP3"
         TXT=${URL}
 elif [[ "${URL}" =~ (\.)([Pp][Nn][Gg]|[Jj][Pp]([Ee])?[Gg]) ]]; then
         LIMIT=10
@@ -118,22 +106,19 @@ elif [[ "${URL}" =~ (\.)([Pp][Nn][Gg]|[Jj][Pp]([Ee])?[Gg]) ]]; then
         done ||\
                 RET=$((RET+1))
 
-        PRE="IMG"
         TXT="bild nedladdad"
 
 elif [[ "${URL}" =~ (\.)([Mm][Pp]4|(Oo][Gg)([Gg]|[Aa]|[Vv]))(\?)?.* ]]; then
 
         FILE=$(echo "$URL" | sed -e s/"^http\(\(.\)*\/\)\+"// | sed s/"\?.*$"//)
-        ${HOME}/src/minwget.sh "$URL" -O "$FILE"
+        "${HOME}"/src/minwget.sh "$URL" -O "$FILE"
 
-        PRE="MP4"
         TXT="film nedladdad"
 else
-        torsocks ${DIR}/youtube.sh "${URL}"   || \
-                ${DIR}/youtube.sh "${URL}"          || \
+        torsocks "${DIR}"/youtube.sh "${URL}"   || \
+                "${DIR}"/youtube.sh "${URL}"          || \
                 RET=$((RET+1))
 
-        PRE="???"
         TXT=$( youtube-dl --get-filename "${URL}" )
 
         RET=$((RET+1))
