2d5d20fdfbc55bdd0bee59a3124678e55cc81057
Johan Öhlin <johanohlin@gmx.com>
2020-03-27 14:15:10 +0100

    some small shell changes

--- sh/ppa.sh
+++ sh/ppa.sh
@@ -7,16 +7,16 @@
 
 # See if there is no argument at all.
 if [ $# -eq 0 ]; then
-    grep ^\#\# $0
-    exit -1
+        grep ^\#\# "$0"
+                exit -1
 fi
 
 # If there was a ppa url or an http url.
-if [ $(echo "$1" | cut -c -3) == "ppa" ]; then
-    TITLE=$(echo "$1" | sed s/ppa\://g | tr "/" " " | gawk '{ print $1 }')
-    PPA="https://launchpad.net/~"$TITLE"/+archive/$(echo "$A" | gawk '{ print $2 }')"
+if [ "$(echo "$1" | cut -c -3)" == "ppa" ]; then
+        TITLE=$(echo "$1" | sed "s/ppa\://g" | tr "/" " " | gawk '{ print $1 }')
+        PPA="https://launchpad.net/~$TITLE/+archive/$(echo "$A" | gawk '{ print $2 }')"
 else
-    PPA="$1"
+        PPA="$1"
 fi
 
 # Get the data will be put into /etc/apt/sources.list. I do not know if lucid is
@@ -24,20 +24,25 @@ fi
 DATA=$(links -dump "$PPA"  | grep -A 1 "deb http" | sed "s/^[ ]*//" | sed s/YOUR_UBUNTU_VERSION_HERE/lucid/)
 
 # Key to be added
-KEY=$(links -dump "$PPA"  | grep -A 1 "Signing key:" | tail -n 1 | tr -d [:blank:] | cut -c 7-14)
+KEY=$(links -dump "$PPA"  | grep -A 1 "Signing key:" | tail -n 1 | tr -d "[:blank:]" | cut -c 7-14)
 
 # Ubuntu's keyserver
 SERVER="apt-key adv --keyserver pgp.mit.edu --recv-keys $KEY"
 
 # If you are root, I will add to sources.list and add GPG keys.
 if [ $UID -eq 0 ]; then
-    $SERVER && \
-    echo -en "\n## $(links -dump "$PPA" | head -n 4 | tail -n 1) PPA added on $(date +"%F %T").\n$DATA" >> /etc/apt/sources.list \
-    || (echo "FAILED" 1>&2 ; exit -1)
-else
-    echo "Run:  $SERVER"
-    echo "and add the following to /etc/apt/sources.list:"
-    echo "$DATA"
-fi
+        if $SERVER; then
+                echo -en "\n## $(links -dump "$PPA" \
+                        | head -n 4 \
+                        | tail -n 1) \
+                        PPA added on $(date +"%F %T").\n$DATA" >> /etc/apt/sources.list \
+                else 
+                        echo "FAILED" 1>&2 ; exit -1
+                fi
+        else
+                echo "Run:  $SERVER"
+                echo "and add the following to /etc/apt/sources.list:"
+                echo "$DATA"
+        fi
 
-exit 0
+        exit 0
--- sh/rensa_lyrics.sh
+++ sh/rensa_lyrics.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 DIR="${HOME}/.lyrics/"
 TMP="${DIR}/tmp/"
 
 mkdir "${TMP}"
-mpc -f "%artist% - %title%.txt" playlist | while read file; do
+mpc -f "%artist% - %title%.txt" playlist | while read -r file; do
     mv -v "${DIR}${file}" "${TMP}";
 done
 
