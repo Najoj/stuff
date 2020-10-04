54c110231e67ed4e234763187f24c13ee9b553fe
Johan Öhlin <johanohlin@gmx.com>
2020-10-04 21:11:04 +0200

    nextsong.sh works again
    Shellcheck approves.

--- sh/nextsong.sh
+++ sh/nextsong.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
 err() {
-    echo "$@" >&2
+    echo "$@" >&2 
 }
+
+REQ=(mpc whiptail)
+for r in "${REQ[@]}"; do
+        if ! command -v "$r" > /dev/null; then
+                err "$r is missing" 
+                exit 1
+        fi
+done
 
 if [ -z "$*" ]; then
     err "No input."
@@ -10,32 +17,45 @@ if [ -z "$*" ]; then
 fi
 
 LIM=20
-RESULT=$(mpc -f "%position% \"%artist% - %title% (%album%)\" off  " playlist | grep -Ei "$@" | shuf | tail -n $LIM )
-LEN=$(echo "${RESULT}" | grep -cv ^$)
+RESULT=()
+while IFS=$'\t' read -r nr title; do
+        RESULT+=("$nr")
+        RESULT+=("$title")
+        RESULT+=("off")
+        ((LEN+=1))
+done < <(mpc -f "%position%\\t%artist% - %title% (%album%)" playlist | \
+        grep -Ei "$@" | shuf | tail -n $LIM | sort -g | sed "s/!/\\!/g" | \
+        sed "s/?/\\?/g")
 
-RESULT=$(echo "${RESULT}" | tr '\n' ' ')
-WHIPTAIL="whiptail --fb --notags --radiolist \"Vilken menar du?\" 30 80 20 $RESULT"
+LEN=${#RESULT[@]}
+LEN=$((LEN/3))
+if [ $LEN -eq 0 ]; then
+        err "No results"
+fi
+
+WHIPTAIL=(whiptail --fb --notags --radiolist "Vilken låt\\?" 30 80 20)
+WHIPTAIL=("${WHIPTAIL[@]}" "${RESULT[@]}")
+echo "$LEN"
 
 if [ "$LEN" -eq 0 ]; then
     err "No results."
     exit 1
 elif [ "$LEN" -eq 1 ]; then
-    FROM=$(echo "$RESULT" | cut -d\  -f 1)
+    FROM=$(echo "${RESULT[0]}" | cut -d\  -f 1)
 else
-    FROM=$($WHIPTAIL 3>&1 1>&2 2>&3)
-    if [ -z $FROM ]; then
-        err "No file chosen."
+    FROM=$(exec "${WHIPTAIL[@]}" 3>&1 1>&2 2>&3)
+    if [ -z "$FROM" ]; then
+        err "No song chosen."
         exit 1
     fi
 fi
 
 TO=$(($(mpc -f "%position%" current)+1))
 
-if [ $FROM -lt $TO ]; then
+if [ "$FROM" -lt "$TO" ]; then
     TO=$((TO+1))
 fi
 
 mpc mv "$FROM" "$TO"
 
-exit
-
+exit $?
--- sh/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./req.sh || exit 1
-
-i=$(require ls hello firefox chrome)
-echo "$i"
-
