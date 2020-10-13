6669807544d04f2d98134aab850b796dae8babc6
Johan Öhlin <johanohlin@gmx.com>
2020-10-13 22:36:41 +0200

    shellsheck

--- sh/veckor_gen.sh
+++ sh/veckor_gen.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Give year as argument.
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     YEAR=$(date +%Y)
 else
     YEAR=$1
 fi
 
-echo -e "YEAR:\t\"$YEAR\"" 1>&2
 ################################################################################
 
 # Is is leap year?
@@ -16,40 +15,33 @@ if date --date="29 February $YEAR" 1>&2 ; then
 else
     FEB=28
 fi
-echo -e "FEB:\t\"$FEB\"" 1>&2
 
 
 ################################################################################
-SW="0"   # Starting week
-SD="0"   # Starting day
+SW=0   # Starting week
+SD=0   # Starting day
 while [ "$SW" != "02" ]; do
-    SD=$(( $SD + 1 ))
+    (( SD += 1 ))
     SW=$(date +%V -d "$SD JAN $YEAR 00:00:00")
 done
 
 if [ $SD -gt 7 ]; then
-    SD=$(( $SD - 7 ))
+    (( SD -= 7 ))
     SW="01"
 fi
 
-echo -e "SW:\t\"$SW\"" 1>&2
-echo -e "SD:\t\"$SD\"" 1>&2
-
 ################################################################################
-EW="52"  # Ending week
-ED="32"  # Ending day
+EW=52  # Ending week
+ED=32  # Ending day
 while [ "$EW" != "51" ]; do
-    ED=$(( $ED - 1 ))
+    (( ED -= 1 ))
     EW=$(date +%V -d "$ED DEC $YEAR 00:00:00")
 done
 
 if [ $ED -lt 25 ]; then
-    ED=$(( $ED + 7 ))
+    (( ED += 7 ))
     EW="52"
 fi
-
-echo -e "EW:\t\"$EW\"" 1>&2
-echo -e "ED:\t\"$ED\"" 1>&2
 
 ################################################################################
 # January
@@ -69,7 +61,6 @@ for i in $(seq -w 1 $ED); do
     SEQ="$SEQ $i"
 done
 
-echo -e "\nSEQ:\t\"$SEQ\"\n" 1>&2
 
 ################################################################################
 # This is where the action happens
@@ -79,13 +70,13 @@ B=17
 C=18
 D=20
 for i in $(seq -w $SW $EW); do
-    echo -en "v$i  $(echo $SEQ | cut -c $A-$B | sed "s/^[ ]*//g")"
-    echo -e  "Rs $(echo $SEQ | cut -c $C-$D | sed "s/^[ ]*//g")Re"
+    echo -en "v$i  $(echo "$SEQ" | cut -c $A-$B | sed "s/^[ ]*//g")"
+    echo -e  "Rs $(echo "$SEQ" | cut -c $C-$D | sed "s/^[ ]*//g")Re"
 
-    let A=$A+21
-    let B=$B+21
-    let C=$C+21
-    let D=$D+21
+    ((A+=21))
+    ((B+=21))
+    ((C+=21))
+    ((D+=21))
 done
 
 exit 0
