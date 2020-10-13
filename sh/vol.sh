cc776106c082568617f1e8f4116f5e3a41390ad7
Johan Öhlin <johanohlin@gmx.com>
2020-10-13 22:38:01 +0200

    shellcheck
    all shell script now satisfies shellcheck, though some have disabled
    rules

--- sh/vol.sh
+++ sh/vol.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #~ while sleep 1; do
-if amixer -c 0 get Master | grep \\[off\\] > /dev/null ; then
+if amixer -c 0 get Master | grep "\\[off\\]" > /dev/null ; then
     echo 0
 else
-    amixer -c 0 get Master | grep Front\ Left\: | awk '{ print $4 }' |  tr -d [:punct:]
+    amixer -c 0 get Master | grep Front\ Left: | awk '{ print $4 }' |  tr -d "[:punct:]"
 fi
 #~ done
 
