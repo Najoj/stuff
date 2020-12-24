a1cca94769ea22fe5dceca9005a9297f9838f97c
Johan Öhlin <johanohlin@gmx.com>
2020-12-24 12:33:27 +0100

    Remove stuck folders and comments
    Remove folders which has not been removed for 24 hours. A download
    should not take longer than that.

--- sh/minsvtget.sh
+++ sh/minsvtget.sh
@@ -5,8 +5,20 @@ TITLE="svtget" # ÄNDRA INTE
 echo -e '\033]2;'$TITLE'\007'
 
 TMP=".svtplay-dl-"
+
+# Remove stuck folders
+AGO=$(date --date="24 hours ago" +%s)
+find ${TMP}* -maxdepth 1 -type d 2> /dev/null | while read -r directory; do
+        NOW=$(stat -c "%Y" "$directory")
+        if [ $((NOW-24*60*60)) -gt "$AGO" ]; then
+                echo "Tar bort $directory"
+                rm -rf "$directory"
+        fi
+done
+
+# See if there are plenty of dowloads already running
 R=1
-while ! find .svtplay-dl-* -maxdepth 1 -type d 2> /dev/null | wc -l | grep -E ^"[0-2]"$; do
+while ! find ${TMP}* -maxdepth 1 -type d 2> /dev/null | wc -l | grep -E ^"[0-2]"$; do
         echo -en '\r'
         date +%T | tr -d '\n'
 
@@ -18,6 +30,7 @@ done
 SHA=$(echo "$URL" | sha256sum | awk '{ print $1 }' )
 TMP="${TMP}${SHA}"
 
+# See if folder already exists
 if [ -d "$TMP" ]; then
         echo "$(date +%T)  Mappen \"$TMP\" finns redan."
         sleep 10
@@ -27,12 +40,14 @@ fi
 mkdir -p "$TMP" 
 cd "$TMP" || exit 1
 
+# Dowload with 3 retries
 R=0
 while ! youtube-dl "${URL}" && [ $R -lt 3 ]; do
         sleep 5
         R=$((R+1))
 done
 
+# Move files and remove files
 if [ $R -lt 3 ]; then
         mv -nv -- *.* ..
         cd .. && rmdir "$TMP"
