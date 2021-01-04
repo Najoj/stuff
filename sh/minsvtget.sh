9be557974d6ce035f419020d3020cb0696b9c324
Johan Öhlin <johanohlin@gmx.com>
2021-01-04 17:14:11 +0100

    better svtget check with pgrep

--- sh/minsvtget.sh
+++ sh/minsvtget.sh
@@ -18,7 +18,7 @@ done
 
 # See if there are plenty of dowloads already running
 R=1
-while ! find ${TMP}* -maxdepth 1 -type d 2> /dev/null | wc -l | grep -E ^"[0-2]"$; do
+while pgrep -ic svtget | grep -E ^"[0-2]"$; do
         echo -en '\r'
         date +%T | tr -d '\n'
 
