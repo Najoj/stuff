670abf486b14738f4b901098b358ce0f2ed47d31
Johan Öhlin <johanohlin@gmx.com>
2021-06-12 21:18:00 +0200

    shorter sleep in wget wait

--- sh/minwget.sh
+++ sh/minwget.sh
@@ -4,7 +4,7 @@
 RET=0
 while ! wget --tries=16 -U "$(cat ~/.useragent)" -c "$@"; do
     echo "Nytt försök om 10 sekunder."
-    sleep 10
+    sleep 5
     RET=$((RET+1))
     if [ $RET -ge 10 ]; then
         break
