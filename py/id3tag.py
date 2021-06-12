c08cd20d8082bd1d51571a9c8ff9fed8b044e806
Johan Öhlin <johanohlin@gmx.com>
2021-06-13 00:56:56 +0200

    changes according to pylint
    10/10

--- py/id3tag.py
+++ py/id3tag.py
@@ -1,15 +1,20 @@
-from mutagen.easyid3 import EasyID3
-import mutagen
+"""
+Remove all tags but artist and title from given file.
+"""
+
 import sys
+import mutagen
 
 if len(sys.argv) < 2:
     sys.exit(0)
 
 for arg in sys.argv[1:]:
     file = mutagen.File(arg)
+    if not file:
+        continue
 
     for tag in file:
-        if tag != 'artist' and tag != 'title':
+        if tag not in ('artist', 'title'):
+            print(f'{arg}: Remove tag {tag}')
             file.pop(tag)
     file.save()
-
