fc5cf5f2a9ec9e09ac6dbc5fa880eb32fe12bbf1
Johan Öhlin <johanohlin@gmx.com>
2021-07-04 15:56:30 +0200

    handle magnet links

--- sh/lad.sh
+++ sh/lad.sh
@@ -9,6 +9,8 @@ URL=$1
 for URL in "$@"; do
    if [[ "${URL}" =~ http(s)?://(www\.)?(aftonbladet|comedycentral|di|dn|dplay|efn|expressen|kanal9play|tv4|svd|nickelodeon|ur|viafree|oppetarkiv|tv10play|tv3play|tv4play|tv6play|tv8play|urplay|svtplay)\.se ]]; then
        $LADDANED "$URL"
+   elif [[ "${URL}" =~ magnet ]]; then
+       $LADDANED "$URL"
    else
        echo "$URL" >> "$FIL"
    fi
