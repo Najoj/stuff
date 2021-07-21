baa686ff4b466c8ccc85d7e1c8df4e15468b5318
Johan Öhlin <johanohlin@gmx.com>
2021-07-21 13:23:44 +0200

    minor browser change

--- sh/nrss.sh
+++ sh/nrss.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$1" =~ http(s)?://(www.)?(youtube|vimeo|sverigesradio|svtplay|efukt) ]]; then
-    "${HOME}/src/lad.sh" "$1"
+        "${HOME}/src/lad.sh" "$1"
 
 elif [[ "$1" =~ http(s)?://.*.(PNG|png|jpg|gif|jpeg) ]]; then
-    feh "$1"
+        feh "$1"
 
 else
-    qutebrowser "$1" &> /dev/null &
+        firefox "$1" &> /dev/null &
 fi
 exit $?
