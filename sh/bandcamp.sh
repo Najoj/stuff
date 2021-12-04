8f7e3fd702dd20412ac1757490528ea5ff35f0a8
Johan Öhlin <johanohlin@gmx.com>
2021-12-04 13:17:21 +0100

    two new scripts

---
+++ sh/bandcamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function manage
+{
+        ((retries=5))
+        ((count=0))
+        echo "$url"
+        while ! youtube-dl -x -ci --audio-format=vorbis "$url"; do
+                if [[ "$retries" -le "$count" ]]; then
+                        break
+                fi
+                ((count++))
+                sleep 5
+        done
+
+        if [[ "$retries" -le "$count" ]]; then
+                return 1
+        fi
+        return 0
+}
+
+r=0
+if [[ "$#" -gt 0 ]]; then
+        for url in "${@}"; do
+                ((r+=$(manage "$url")))
+        done
+else
+        echo "Write your urls. End with ^D."
+        while read -r url; do
+                ((r+=$(manage "$url")))
+        done 
+fi
+
+exit "$r"
---
+++ sh/julmusik.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+JULLISTA="/media/musik/.spellistor/jullista.m3u"
+SPELA_KLART="${HOME}/src/spela_klart"
+
+reqs=("$JULLISTA" "$SPELA_KLART")
+for req in "${reqs[@]}"; do
+        if ! [[ -e "$req" ]]; then
+                >&2 echo "Filen finns inte: $req"
+                exit 1
+        fi
+done
+
+reqs=(mpc flock)
+for req in "${reqs[@]}"; do
+        if ! command -v "$req" &> /dev/null; then
+                >&2 echo "Programmet finns inte: $req"
+                exit 1
+        fi
+done
+
+MPC_LOCK="/home/jojan/.mpc_lock"
+sort -u "$JULLISTA" | shuf | while read -r file; do
+        (flock -x 9 
+        echo "$file"
+        if [[ -e "/media/musik/$file" ]]; then
+                mpc insert "$file"
+                "$SPELA_KLART"
+                POS=$(mpc -f "%position%" current) 
+                "$SPELA_KLART"
+                mpc del "$POS"
+        else 
+                >&2 echo "Tog bort $file"
+                sed -i "/$file/d" "$JULLISTA"
+        fi) 9> "$MPC_LOCK"
+
+        for _ in {1..10}; do
+                "$SPELA_KLART"
+        done
+done
+
