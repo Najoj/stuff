36003c268352079f68dc535cb327b1f9a075ee92
Johan Öhlin <johanohlin@gmx.com>
2022-01-13 19:30:25 +0100

    fix: refactor and test and new feature for id3 tag script

--- py/id3tag.py
+++ py/id3tag.py
@@ -2,37 +2,87 @@
 #-*- coding:utf-8 -*-
 
 """
-Remove all tags but artist and title from given file. Only works for Vorbis
+Remove all tags but artist and title from given file. Only tested with Vorbis
 files.
 """
 
-import sys
 import mutagen
+import os
+import sys
+
+
+def parent(word: str, first_word: bool) -> str:
+    """
+    Manages parenthised words
+
+    >>> parent('(The', True)
+    '(The'
+    >>> parent('(The', False)
+    '(the'
+    >>> parent('Woman)', True)
+    'Woman)'
+    >>> parent('Woman)', False)
+    'Woman)'
+    >>> parent('The', True)
+    'The'
+    >>> parent('The', False)
+    'the'
+    >>> parent('(the)', True)
+    '(The)'
+    >>> parent('(the)', False)
+    '(the)'
+    """
+    lparent, rparent = word[0] == '(', word[-1] == ')'
+
+    if lparent:
+        word = word[1:]
+    if rparent:
+        word = word[:-1]
+
+    word = word.lower()
+
+    lowercased = ('a', 'an', 'in', 'of', 'on', 'the', 'and', 'at')
+    cap = first_word or word not in lowercased
+    if cap:
+        word = word.capitalize()
+
+    if lparent:
+        word = f'({word}'
+    if rparent:
+        word = f'{word})'
+
+    return word
 
 
 def capitalise(lang: str, tag: str) -> str:
     """
     Capitalise tag according to lang option
 
-    >>> capitalise('', 'the mAn in A cave OF Love')
+    >>> capitalise('', ' the mAn in A cave OF Love ')
     'the mAn in A cave OF Love'
     >>> capitalise('e', 'the mAn in A cave OF Love')
-    'The Man In a Cave of Love'
+    'The Man in a Cave of Love'
     >>> capitalise('s', 'the Man In A Cave OF Love')
     'The man in a cave of love'
+    >>> capitalise('', 'it´s a Man In A Cave OF Love')
+    "it's a Man In A Cave OF Love"
+    >>> capitalise('e', 'it´s a Man In A Cave OF Love')
+    "It's a Man in a Cave of Love"
+    >>> capitalise('s', 'it´s a Man In A Cave OF Love')
+    "It's a man in a cave of love"
     """
     capitalised = tag
 
     if lang == 'e':
         words = tag.split()
-        capitalised = words[0].capitalize()
+
+        word = words[0]
+        word = parent(word, True)
+        capitalised = word
+
         for word in words[1:]:
             word = word.lower()
-            if word not in ('of', 'the', 'a', 'an', '(feat'):
-                if word[0] == '(':
-                    word = '(' + word[1:].capitalize()
-                else:
-                    word = word.capitalize()
+            word = parent(word, False)
             capitalised += f' {word}'
 
     elif lang == 's':
@@ -40,7 +90,7 @@ def capitalise(lang: str, tag: str) -> str:
         capitalised = words[0].capitalize()
         capitalised += ' ' + ' '.join(x.lower() for x in words[1:])
 
-    return capitalised.strip()
+    return capitalised.strip().replace("´", "'")
 
 
 def main():
@@ -60,14 +110,35 @@ def main():
             file = mutagen.File(arg)
             if not file:
                 continue
-
+            
+            changed = False
             for tag in file:
                 if tag not in ('artist', 'title'):
                     print(f'{arg}: Remove tag {tag}')
                     file.pop(tag)
                 else:
-                    file[tag] = capitalise(lang, file[tag][0])
+                    before = file[tag][0]
+                    after = capitalise(lang, before)
+                    file[tag] = after
+                    if before != after:
+                        changed = True
+                        print(f'{before} -> {after}')
+
             file.save()
+
+            if changed:
+                # New filename
+                ext = arg.split('.')[-1]
+                filename = file['artist'][0] + ' - ' + file['title'][0] + '.' + ext
+                
+                answer = ''
+                while answer not in ('y', 'n'):
+                    answer = input(f'Move {arg} to {filename}? [y/n]').lower()
+                if answer == 'y':
+                    try:
+                        os.rename(arg, filename)
+                    except os.OSError:
+                        print(f'Could write to {filename}.')
 
 
 if '__main__' == __name__:
