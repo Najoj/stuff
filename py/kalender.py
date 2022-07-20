c7f60ded4a6a1e8ff5561fdc50d2ba121d3e3fc8
Johan Öhlin <johanohlin@gmx.com>
2022-07-21 01:33:17 +0200

    Print flag days as well

--- py/kalender.py
+++ py/kalender.py
@@ -4,9 +4,9 @@ Generate some dates to be added in Calcurse's apts file.
 
 import calendar
 import collections
+import copy
 import datetime
 import math
-import copy
 
 veckodagar = mån, tis, ons, tor, fre, lör, sön = range(0, 7)
 månader = jan, feb, mar, apr, maj, jun, jul, aug, sep, okt, nov, dec = range(1, 13)
@@ -59,13 +59,17 @@ def get_day_of_week(year, month, day):
 
 
 class Date:
-    def __init__(self, year, month, day, description=None):
+    def __init__(self, year, month, day,
+                 description=None, red=False, flag=False):
         self.year = year
         self.month = month
         self.day = day
-        self.description = description
 
-        self.day_of_week = get_day_of_week(year, month, day)
+        self.description = description
+        self.red_day = red
+        self.flag_day = flag
+
+        self.day_of_week = 0 if year == 0 else get_day_of_week(year, month, day)
 
         _is_leap_year = skottår(year)
         self._days_in_month = {jan: 31,
@@ -74,7 +78,7 @@ class Date:
 
     def __add__(self, other: int):
         self.day += other
-        self.day_of_week = (self.day_of_week + other) % 7
+        self.day_of_week = (self.day_of_week + other) % len(veckodagar)
         if self.day > self._days_in_month[self.month]:
             self.day -= self._days_in_month[self.month]
             self.month += 1
@@ -88,7 +92,7 @@ class Date:
 
     def __sub__(self, other):
         self.day -= other
-        self.day_of_week = (self.day_of_week - other + 7) % 7
+        self.day_of_week = (self.day_of_week - other) % len(veckodagar)
         if self.day < 0:
             self.month -= 1
             self.day += self._days_in_month[self.month]
@@ -105,32 +109,56 @@ class Date:
         d = self.day
         y = self.year
         desc = self.description
-        return f'{m}/{d}/{y} [1] {desc} (röd dag)'
+
+        # If year was set to
+        yearly = ' {1Y} ' if y == 0 else ' '
+        y = 1970 if y == 0 else y
+
+        if self.red_day and self.flag_day:
+            desc += ' (röd dag och flaggdag)'
+        elif self.red_day:
+            desc += ' (röd dag)'
+        elif self.flag_day:
+            desc += ' (flaggdag)'
+
+        return f'{m}/{d}/{y} [1]{yearly}{desc}'
 
 
-def skärtorsdagen(year: int, description='') -> Date:
+def skärtorsdagen(year: int) -> Date:
     # Torsdagen före Påskdagen
     _skärtorsdagen = påskdagen(year)
     _skärtorsdagen -= (sön - tor)
-    _skärtorsdagen.description = description
+
+    _skärtorsdagen.description = 'Skärtorsdagen'
+    _skärtorsdagen.flag_day = False
+    _skärtorsdagen.red_day = True
+
     assert _skärtorsdagen.day_of_week == tor
     return _skärtorsdagen
 
 
-def långfredagen(year: int, description='') -> Date:
+def långfredagen(year: int) -> Date:
     # Fredagen före Påskdagen
     _långfredagen = påskdagen(year)
     _långfredagen -= (sön - fre)
-    _långfredagen.description = description
+
+    _långfredagen.description = 'Långfredgan'
+    _långfredagen.flag_day = False
+    _långfredagen.red_day = True
+
     assert _långfredagen.day_of_week == fre, _långfredagen.day_of_week
     return _långfredagen
 
 
-def påskafton(year: int, description='') -> Date:
+def påskafton(year: int) -> Date:
     # Lördagen före Påskdagen.
     _påskafton = påskdagen(year)
     _påskafton -= (sön - lör)
-    _påskafton.description = description
+
+    _påskafton.description = 'Påskafton'
+    _påskafton.flag_day = False
+    _påskafton.red_day = True
+
     assert _påskafton.day_of_week == lör
     return _påskafton
 
@@ -138,7 +166,7 @@ def påskafton(year: int, description='') -> Date:
 _påskdagen = None
 
 
-def påskdagen(year: int, description='') -> Date:
+def påskdagen(year: int) -> Date:
     # Första söndagen efter ecklesiastisk fullmåne, efter vårdagjämningen
     global _påskdagen
     if _påskdagen is None:
@@ -167,120 +195,196 @@ def påskdagen(year: int, description='') -> Date:
             else:
                 _påskdagen = Date(year, mar, days)
 
-    _påskdagen.description = description
+    _påskdagen.description = 'Påskdagen'
+    _påskdagen.flag_day = True
+    _påskdagen.red_day = True
+
     assert _påskdagen.day_of_week == sön, _påskdagen.day_of_week
     return copy.copy(_påskdagen)
 
 
-def annandag_påsk(year: int, description='') -> Date:
+def annandag_påsk(year: int) -> Date:
     # Dagen efter påskdagen
     _annandag_påsk = påskdagen(year)
     _annandag_påsk += 1
-    _annandag_påsk.description = description
+
+    _annandag_påsk.description = 'Annandag påsk'
+    _annandag_påsk.red_day = True
+    _annandag_påsk.flag_day = False
+
     return _annandag_påsk
 
 
-def kristi_himmelfärdsdag(year: int, description='') -> Date:
+def kristi_himmelfärdsdag(year: int) -> Date:
     # Sjätte torsdagen efter påskdagen
     _kristi_himmelfärdsdag = påskdagen(year)
-    sixth = 0
+
+    sixth = 1 if _kristi_himmelfärdsdag.day_of_week == tor else 0
     increment = 1
     while sixth != 6:
         _kristi_himmelfärdsdag += increment
         if _kristi_himmelfärdsdag.day_of_week == tor:
             sixth += 1
-            increment = 7
-    _kristi_himmelfärdsdag.description = description
+            increment = len(veckodagar)
+
+    _kristi_himmelfärdsdag.description = 'Kristi himmelfärdsdag'
+    _kristi_himmelfärdsdag.red_day = True
+    _kristi_himmelfärdsdag.flag_day = False
+
     return _kristi_himmelfärdsdag
 
 
-def pingstafton(year: int, description='') -> Date:
+def pingstafton(year: int) -> Date:
     # Dagen före pingstdagen
     _pingstafton = pingstdagen(year)
     _pingstafton -= 1
-    _pingstafton.description = description
+
+    _pingstafton.description = 'Pingstafton'
+    _pingstafton.red_day = True
+    _pingstafton.flag_day = False
+
     return _pingstafton
 
 
-def pingstdagen(year: int, description='') -> Date:
+def pingstdagen(year: int) -> Date:
     # Sjunde söndagen efter påskdagen
     _pingstdagen = påskdagen(year)
-    seventh = 0
+    seventh = 1 if _pingstdagen.day_of_week == sön else 0
     increment = 1
     while seventh != 7:
         _pingstdagen += increment
         if _pingstdagen.day_of_week == sön:
             seventh += 1
-            increment = 7
-    _pingstdagen.description = description
+            increment = len(veckodagar)
+
+    _pingstdagen.description = 'Pingstdagen'
+    _pingstdagen.red_day = True
+    _pingstdagen.flag_day = True
+
     return _pingstdagen
 
 
-def midsommarafton(year: int, description='') -> Date:
+def midsommarafton(year: int) -> Date:
     # Fredagen mellan 19 juni och 25 juni (fredagen före midsommardagen)
     _midsommarafton = midsommardagen(year)
     _midsommarafton -= 1
-    _midsommarafton.description = description
+
+    _midsommarafton.description = 'Midsommmarafton'
+    _midsommarafton.red_day = True
+    _midsommarafton.flag_day = False
+
     return _midsommarafton
 
 
-def midsommardagen(year: int, description='') -> Date:
+def midsommardagen(year: int) -> Date:
     # Lördagen mellan 20 juni och 26 juni
     _midsommardagen = Date(year, jun, 20)
     while _midsommardagen.day_of_week != lör:
         _midsommardagen += 1
-    _midsommardagen.description = description
+
+    _midsommardagen.description = 'Midsommardagen'
+    _midsommardagen.red_day = True
+    _midsommardagen.flag_day = True
+
     return _midsommardagen
 
 
-def allhelgonaafton(year: int, description='') -> Date:
+def allhelgonaafton(year: int) -> Date:
     # Fredag mellan 30 oktober och 5 november
     _allhelgonaafton = alla_helgons_dag(year)
     _allhelgonaafton -= 1
-    _allhelgonaafton.description = description
+
+    _allhelgonaafton.description = 'Allhelgonaafton'
+    _allhelgonaafton.red_day = True
+    _allhelgonaafton.flag_day = False
+
     return _allhelgonaafton
 
 
-def alla_helgons_dag(year: int, description='') -> Date:
+def alla_helgons_dag(year: int) -> Date:
     # Lördagen som infaller under perioden från 31 oktober till 6 november
     _alla_helgons_dag = Date(year, okt, 31)
     while _alla_helgons_dag.day_of_week != lör:
         _alla_helgons_dag += 1
-    _alla_helgons_dag.description = description
+
+    _alla_helgons_dag.description = 'Alla helgons dag'
+    _alla_helgons_dag.red_day = True
+    _alla_helgons_dag.flag_day = False
+
     return _alla_helgons_dag
 
+
+def valår(year):
+    return 2 == year % 4
+
+
+def valdagen(year):
+    # Andra söndagen i september, vid valår
+    _valdagen = Date(year, sep, 1, description='Valdagen', red=False, flag=True)
+
+    second = 1 if _valdagen.day_of_week == sön else 0
+    increment = 1
+    while second != 2:
+        _valdagen += increment
+        if _valdagen.day_of_week == sön:
+            second += 1
+            increment = len(veckodagar)
+
+    return _valdagen
 
 def main():
     calendar.date = collections.namedtuple('Date', ['year', 'month', 'day'])
     år = datetime.datetime.now().date().strftime('%Y')
     år = int(år)
 
-    red_days = [
-        # Date(år, jan, 1),  # Nyårsdagen
-        # Date(år, jan, 5),  # Trettondagsafton
-        # Date(år, jan, 6),  # Trettondedag jul
-        skärtorsdagen(år, 'Skärtorsdagen'),
-        långfredagen(år, 'Långfredgan'),
-        påskafton(år, 'Påskafton'),
-        påskdagen(år, 'Påskdagen'),
-        annandag_påsk(år, 'Annandag påsk'),
-        # Date(år, apr, 30),  # Valborgsmässoafton
-        # Date(år, maj, 1),  # Första maj
-        kristi_himmelfärdsdag(år, 'Kristi himmelfärdsdag'),
-        pingstafton(år, 'Pingstafton'),
-        pingstdagen(år, 'Pingstdagen'),
-        # Date(år, jun, 6),  # Sveriges nationaldag
-        midsommarafton(år, 'Midsommmarafton'),
-        midsommardagen(år, 'Midsommardagen'),
-        allhelgonaafton(år, 'Allhelgonaafton'),
-        alla_helgons_dag(år, 'Alla helgons dag'),
-        # Date(år, dec, 24),  # Julafton
-        # Date(år, dec, 25),  # Juldagen
-        # Date(år, dec, 26),  # Annandag jul
-        # Date(år, dec, 31),  # Nyårsafton
+    dates = [
+        Date(0, jan, 1, description='Nyårsdagen', red=True, flag=True),
+        Date(0, jan, 5, description='Trettondagsafton', red=True, flag=False),
+        Date(0, jan, 6, description='Trettondedag jul', red=True, flag=False),
+        Date(0, jan, 28, description='Konungens namnsdag', red=False, flag=True),
+
+        skärtorsdagen(år),
+        långfredagen(år),
+        påskafton(år),
+        påskdagen(år),
+        annandag_påsk(år),
+
+        Date(år, apr, 30, description='Valborgsmässoafton', red=True, flag=False),
+        Date(0, apr, 30, description='Konungens födelsedag', red=False, flag=True),
+
+        Date(år, maj, 1, description='Första maj', red=True, flag=True),
+        Date(0, maj, 29, description='Veterandagen', red=False, flag=True),
+
+        kristi_himmelfärdsdag(år),
+        pingstafton(år),
+        pingstdagen(år),
+
+        Date(år, jun, 6, description='Sveriges nationaldag', red=True, flag=True),
+
+        midsommarafton(år),
+        midsommardagen(år),
+
+        Date(0, jul, 14, description='Kronprinsessans födelsedag', red=False, flag=True),
+        Date(0, aug, 8, description='Drottningens namnsdag', red=False, flag=True),
+        Date(0, okt, 24, description='FN-dagen', red=False, flag=True),
+        Date(0, nov, 6, description='Gustav Adolfsdagen', red=False, flag=True),
+
+        allhelgonaafton(år),
+        alla_helgons_dag(år),
+
+        Date(0, dec, 10, description='Nobeldagen', red=False, flag=True),
+
+        Date(0, dec, 23, description='Drottningens födelsedag', red=False, flag=True),
+        Date(år, dec, 24, description='Julafton', red=True, flag=False),
+        Date(år, dec, 25, description='Juldagen', red=True, flag=True),
+        Date(år, dec, 26, description='Annandag jul', red=True, flag=False),
+        Date(år, dec, 31, description='Nyårsafton', red=True, flag=False),
     ]
-    for date in red_days:
+    for date in dates:
         print(date)
+
+    if valår(år):
+        print(valdagen(år))
 
 
 if __name__ == '__main__':
