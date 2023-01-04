70a9d88972e80186062f18a364e401f893691341
Johan Öhlin <johanohlin@gmx.com>
2023-01-04 14:22:06 +0100

    made use of until insead of while !

--- sh/minwget.sh
+++ sh/minwget.sh
@@ -2,7 +2,7 @@
 
 
 RET=0
-while ! wget --tries=16 -U "$(cat ~/.useragent)" -c "$@"; do
+until wget --tries=16 -U "$(cat ~/.useragent)" -c "$@"; do
     echo "Nytt försök om 10 sekunder."
     sleep 5
     RET=$((RET+1))
