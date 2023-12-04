4fd51badff2fc05b7ef97faeaef6b10cac003b18
Johan Öhlin <johanohlin@gmx.com>
2023-12-04 21:34:59 +0100

    misc bigger changes

--- sh/julmusik.sh
+++ sh/julmusik.sh
@@ -3,7 +3,23 @@
 # List of directories relative mpd's music_directory.
 JULLISTA="/media/musik/.spellistor/jullista.m3u"
 SPELA_KLART="${HOME}/src/spela_klart"
-FREQ=20
+
+MONTH=$(date +%m)
+if [[ $MONTH -lt 12 ]]; then
+        echo "Inte ens december, dumsnut!"
+        exit 1
+fi
+
+
+DAY=$(date +%d)
+if [[ $DAY -lt 12 ]]; then
+        FREQ=40
+elif [[ $DAY -lt 24 ]]; then
+        FREQ=20
+else
+        echo "Julafton har redan varit, dumbom!"
+        exit 1
+fi
 
 reqs=("$JULLISTA" "$SPELA_KLART")
 for req in "${reqs[@]}"; do
@@ -22,9 +38,12 @@ for req in "${reqs[@]}"; do
 done
 
 MPC_LOCK="/home/jojan/.mpc_lock"
+N=$(sort -u "$JULLISTA" | wc -l)
+I=0
 sort -u "$JULLISTA" | shuf | while read -r file; do
+        ((I++))
         (flock -x 9 
-        echo "$file"
+        echo "$I / $N. $file"
         if [[ -e "/media/musik/$file" ]]; then
                 mpc insert "$file"
                 "$SPELA_KLART"
@@ -36,9 +55,11 @@ sort -u "$JULLISTA" | shuf | while read -r file; do
                 sed -i "/$file/d" "$JULLISTA"
         fi) 9> "$MPC_LOCK"
 
-        for ((i=0; i<FREQ; i++)); do
-                "$SPELA_KLART"
-        done
+        if [[ -e "/media/musik/$file" ]]; then
+                for ((i=0; i<FREQ; i++)); do
+                        "$SPELA_KLART" && echo -n .
+                done
+        fi
 done
 
 echo "Slut!"
--- sh/ladda_ned.sh
+++ sh/ladda_ned.sh
@@ -1,78 +1,96 @@
 #!/bin/bash
 
+
+UTILS=${HOME}/src/utils.sh
+if ! [[ -e  "${UTILS}" ]]; then
+        exit 1
+fi
+source "$UTILS"
+
 DIR="${HOME}/src/ln"
 RET=0
 
+if ! required_programs deluge-console youtube-dl trurl; then
+        exit 1
+fi
+
 for URL in "$@"; do
         PRET=$RET
+        if ! trurl --verify "$URL"; then
+                print_warning "\"$URL\" is invalid"
+                break
+        fi
+
         if [[ "${URL}" =~ magnet:\? ]] ||
                 [[ "${URL}" =~ http(s)?://(.*)\.torrent ]]; then
                 deluge-console "add ${URL}"
                 TXT="${URL}"
-
-        elif [[ "${URL}" =~ http(s?):\/\/open\.spotify\.com\/(artist|track|album)\/[A-Za-z0-9]{10}(\\?.*)? ]]; then
-                     "${DIR}"/spotify.sh "${URL}" || RET=$((RET + 1))
-                     if [ "$?" == 3 ]; then
-                             RET=$((RET - 1))
-                     fi
-                     TXT=${URL}
-
-        elif [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/podrss/[0-9]+(\.|\\-)[mM][pP]3 ]] || \
-             [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/srse/[0-9]+(\.|\\-)[mM][pP]3 ]]; then
-                     "${DIR}"/sr.sh "${URL}" || RET=$((RET + 1))
-                     if [ "$?" == 3 ]; then
-                             RET=$((RET - 1))
-                     fi
-                     TXT=${URL}
-
-        elif [[ "${URL}" =~ http(s)?://(www\.)?(aftonbladet|comedycentral|di|dn|dplay|efn|expressen|kanal9play|tv4|svd|nickelodeon|ur|oppetarkiv|tv10play|tv3play|tv4play|tv6play|tv8play|urplay|svtplay)\.se ]]; then
-                export DISPLAY=":0"
-                urxvt -title 'svtget' -cd "$(pwd)" -e "${DIR}"/minsvtget.sh "${URL}" &
-                disown
-                TXT="Öppnades i egent fönster: $?"
-                sleep 1
-
-        elif [[ "${URL}" =~ (\.)[mM][pP]3 ]]; then
-                "${DIR}"/sr.sh "${URL}" || RET=$((RET + 1))
-                TXT=${URL}
-
-        elif [[ "${URL}" =~ (\.)([Pp][Nn][Gg]|[Jj][Pp]([Ee])?[Gg]) ]]; then
-                LIMIT=10
-                TIME=30
-                i=0
-                FILE="$(date +%F-%T)-${RANDOM}.png"
-                until wget -c "${URL}" -O- | convert - "${FILE}" && $LIMIT -le $i; do
-                        echo - en "försöker igen... om $TIME ($i/$LIMIT)"
-                        i=$((i + 1))
-                done || RET=$((RET + 1))
-                TXT="bild nedladdad"
-
-        elif [[ "${URL}" =~ (\.)([Oo][Gg][Gg]|[Ff][Ll][Aa][Cc]) ]]; then
-                "${HOME}"/src/minwget.sh "$URL"
-                TXT="ljud nedladdad"
-
-        elif [[ "${URL}" =~ (\.)([Zz][Ii][Pp])(\?)?.* ]] ||
-                [[ "${URL}" =~ (\.)([Tt][Aa][Rr](\,[Gg][Zz])?) ]]; then
-                FILE=$(echo "$URL" | sed -e s/"^http\\(\\(.\\)*\\/\\)\\+"// | sed s/"\\?.*$"//)
-                "${HOME}"/src/minwget.sh "$URL" -O "$FILE"
-                TXT="film nedladdad"
-
-        elif [[ "${URL}" =~ (\.)([Mm][Pp]4|(Oo][Gg)([Gg]|[Aa]|[Vv]))(\?)?.* ]]; then
-                FILE=$(echo "$URL" | sed -e s/"^http\\(\\(.\\)*\\/\\)\\+"// | sed s/"\\?.*$"//)
-                "${HOME}"/src/minwget.sh "$URL" -O "$FILE"
-                TXT="film nedladdad"
         else
-                DURATION=$(youtube-dl --get-duration "$URL")
-                if [[ "$DURATION" =~ ^(.:)?..$ ]]; then
-                        "${DIR}"/minyoutube-dl.sh "${URL}" || RET=$((RET+1))
-                        TXT=$(youtube-dl --get-filename "${URL}")
-                else
+                URL=$(trurl --set scheme=https "$URL")
+                if [[ "${URL}" =~ http(s?):\/\/open\.spotify\.com\/(artist|track|album)\/[A-Za-z0-9]{10}(\\?.*)? ]]; then
+                             "${DIR}"/spotify.sh "${URL}" || RET=$((RET + 1))
+                             if [ "$?" == 3 ]; then
+                                     RET=$((RET - 1))
+                             fi
+                             TXT=${URL}
+
+                elif [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/podrss/[0-9]+(\.|\\-)[mM][pP]3 ]] || \
+                     [[ "${URL}" =~ http(s)?://sverigesradio\.se/topsy/ljudfil/srse/[0-9]+(\.|\\-)[mM][pP]3 ]]; then
+                             "${DIR}"/sr.sh "${URL}" || RET=$((RET + 1))
+                             if [ "$?" == 3 ]; then
+                                     RET=$((RET - 1))
+                             fi
+                             TXT=${URL}
+
+                elif [[ "${URL}" =~ http(s)?://(www\.)?(aftonbladet|comedycentral|di|dn|dplay|efn|expressen|kanal9play|tv4|svd|nickelodeon|ur|oppetarkiv|tv10play|tv3play|tv4play|tv6play|tv8play|urplay|svtplay)\.se ]]; then
                         export DISPLAY=":0"
-                        urxvt -title 'youtube-dl' -cd "$(pwd)" -e "${DIR}"/minyoutube-dl.sh "${URL}" &
+                        urxvt -title 'svtget' -cd "${HOME}" -e "${DIR}"/minsvtget.sh "${URL}" &
                         disown
                         TXT="Öppnades i egent fönster: $?"
-                fi
+                        sleep 1
 
+                elif [[ "${URL}" =~ (\.)[mM][pP]3 ]]; then
+                        "${DIR}"/sr.sh "${URL}" || RET=$((RET + 1))
+                        TXT=${URL}
+
+                elif [[ "${URL}" =~ (\.)([Pp][Nn][Gg]|[Jj][Pp]([Ee])?[Gg]) ]]; then
+                        LIMIT=10
+                        TIME=30
+                        i=0
+                        FILE="$(date +%F-%T)-${RANDOM}.png"
+                        until wget -c "${URL}" -O- | convert - "${FILE}" && $LIMIT -le $i; do
+                                echo - en "försöker igen... om $TIME ($i/$LIMIT)"
+                                i=$((i + 1))
+                        done || RET=$((RET + 1))
+                        TXT="bild nedladdad"
+
+                elif [[ "${URL}" =~ (\.)([Oo][Gg][Gg]|[Ff][Ll][Aa][Cc]) ]]; then
+                        "${HOME}"/src/minwget.sh "$URL"
+                        TXT="ljud nedladdad"
+
+                elif [[ "${URL}" =~ (\.)([Zz][Ii][Pp])(\?)?.* ]] ||
+                        [[ "${URL}" =~ (\.)([Tt][Aa][Rr](\,[Gg][Zz])?) ]]; then
+                        FILE=$(echo "$URL" | sed -e s/"^http\\(\\(.\\)*\\/\\)\\+"// | sed s/"\\?.*$"//)
+                        "${HOME}"/src/minwget.sh "$URL" -O "$FILE"
+                        TXT="film nedladdad"
+
+                elif [[ "${URL}" =~ (\.)([Mm][Pp]4|(Oo][Gg)([Gg]|[Aa]|[Vv]))(\?)?.* ]]; then
+                        FILE=$(echo "$URL" | sed -e s/"^http\\(\\(.\\)*\\/\\)\\+"// | sed s/"\\?.*$"//)
+                        "${HOME}"/src/minwget.sh "$URL" -O "$FILE"
+                        TXT="film nedladdad"
+                else
+                        DURATION=$(youtube-dl --get-duration "$URL")
+                        if ! [[ "$DURATION" =~ ^(.+:)*([1-9].:)?..$ ]]; then
+                                "${DIR}"/minyoutube-dl.sh "${URL}" || RET=$((RET+1))
+                                TXT=$(youtube-dl --get-filename "${URL}")
+                        else
+                                export DISPLAY=":0"
+                                urxvt -title 'youtube-dl' -cd "${HOME}" -e "${DIR}"/minyoutube-dl.sh "${URL}" &
+                                disown
+                                TXT="Öppnades i egent fönster: $?"
+                        fi
+
+                fi
         fi
 
         TXT="$(echo "$TXT" | cut -c -80)"
