f6a1ac5b9409d563142259b9e3fa0e9085a7f463
Johan Öhlin <johanohlin@gmx.com>
2024-09-12 22:24:51 +0200

    Add simple int argument for number of songs

--- c/spela_klart.c
+++ c/spela_klart.c
@@ -5,65 +5,86 @@
  *
  * Exits successfully when song changes.
  *
- * Error handling is done by ignoring none-fatal erros and trying to reconnect if
+ * Error handling is done by ignoring none-fatal erros, and try to reconnect if
  * fatal errors happen.
  *
- * TODO: Comment the hell out of the code.
+ * Can take no argument or one argument interpreted as an integer.
+ *
  * TODO: Get host and port from arguments.
  *
  **/
 #include <mpd/client.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+
+#define  print_usage  fprintf (stderr, "Usage: %s [ITERATIONS]\n", argv[0]);
 
 int main (argc, argv)
-	const int argc;
-	const char *const *const argv;
+        const int argc;
+        const char *const *const argv;
 {
-    if (argc > 1)
-    {
-        printf ("Usage: %s\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    int first_song_id = -1;
-    int second_song_id = -1;
-
-    struct mpd_connection *client = mpd_connection_new ("localhost", 6600, 2000);
-    struct mpd_status *status = mpd_run_status (client);
-
-    if (status != NULL)
-    {
-        first_song_id = mpd_status_get_song_id (status);
-        second_song_id = first_song_id;
-    }
-
-    while (first_song_id == second_song_id)
-    {
-        int code;
-        if ((code = mpd_run_idle (client)))
+        /* Wrong number of arguments */
+        if (argc > 2)
         {
-            status = mpd_run_status (client);
-            if (status != NULL)
-            {
-                second_song_id = mpd_status_get_song_id (status);
-            }
+                print_usage;
+                return EXIT_FAILURE;
         }
-        else
-        {
-            if (client != NULL)
-            {
-                mpd_connection_free (client);
-            }
-            fprintf(stderr, "No reply from server. Return code: %d\n", code);
-            return EXIT_FAILURE;
-        }
-    }
 
-    if (client != NULL)
-    {
-        mpd_connection_free (client);
-    }
-    return EXIT_SUCCESS;
+        /* Parse iterations */
+        int iterations = 1; /* Defaults to 1 iteration */
+        int it;
+        if (argc == 2)
+        {
+                it = atoi(argv[1]);
+                if (it < 0)
+                {
+                        print_usage;
+                        return EXIT_FAILURE;
+                }
+                iterations = it;
+        }
+
+        for (it = 0; it < iterations; it++)
+        {
+                int first_song_id = -1;
+                int second_song_id = -1;
+
+                struct mpd_connection *client = mpd_connection_new ("localhost", 6600, 2000);
+                struct mpd_status *status = mpd_run_status (client);
+
+                if (status != NULL)
+                {
+                        first_song_id = mpd_status_get_song_id (status);
+                        second_song_id = first_song_id;
+                }
+
+                while (first_song_id == second_song_id)
+                {
+                        int code;
+                        if ((code = mpd_run_idle (client)))
+                        {
+                                status = mpd_run_status (client);
+                                if (status != NULL)
+                                {
+                                        second_song_id = mpd_status_get_song_id (status);
+                                }
+                        }
+                        else
+                        {
+                                if (client != NULL)
+                                {
+                                        mpd_connection_free (client);
+                                }
+                                fprintf(stderr, "No reply from server. Return code: %d\n", code);
+                                return EXIT_FAILURE;
+                        }
+                }
+                if (client != NULL)
+                {
+                        mpd_connection_free (client);
+                }
+        }
+
+        return EXIT_SUCCESS;
 }
