31be967ecbf2ef35bcd1b9e006029cc53f91fe58
Johan Öhlin <johanohlin@gmx.com>
2024-10-02 19:51:03 +0200

    less verbose

--- sh/rensa_lyrics.sh
+++ sh/rensa_lyrics.sh
@@ -4,32 +4,34 @@
 DIR="${HOME}/.lyrics"
 TMP="$(mktemp -d)"
 
-du "${DIR}"/* | sort -g | grep ^0 | cut -f 2 | while read -r file; do
+du "${DIR}"/* | sort -g | grep -q ^0 | cut -f 2 | while read -r file; do
     rm -vf "$file"
 done
 
 for txt in "${DIR}/"*.txt; do
         # Remove special case, or
         # remove rediculously long files
-        if grep ^Cancel "$txt"; then
-                rm -v "$txt"
-        elif grep 'How to Fromat Lyrics' "$txt"; then
-                rm -v "$txt"
-        elif grep '1 Cancel' "$txt"; then
-                rm -v "$txt"
-        elif wc -l "$txt" | grep -E "^[1-9][0-9]{2,}(.+)\.txt"; then
-                rm -v "$txt"
+        if grep -q ^Cancel "$txt"; then
+                rm "$txt"
+        elif grep -q 'How to Fromat Lyrics' "$txt"; then
+                rm "$txt"
+        elif grep -q '1 Cancel' "$txt"; then
+                rm "$txt"
+        elif wc -l "$txt" | grep -qE "^[1-9][0-9]{2,}(.+)\.txt"; then
+                rm "$txt"
+        elif head -1 "$txt" | grep -q 'Contributors'; then
+                rm "$txt"
         fi
 done
 
 # Save files which has songs in mpd
 mkdir "${TMP}" 2> /dev/null
 mpc -f "%artist% - %title%.txt" playlist | while read -r file; do
-    mv -v "${DIR}/${file}" "${TMP}" 2> /dev/null
+    mv "${DIR}/${file}" "${TMP}" 2> /dev/null
 done
 
-rm -rv "${DIR}"/*.txt
-mv -v "${TMP}"/*.txt "${DIR}"
+rm -r "${DIR}"/*.txt
+mv    "${TMP}"/*.txt "${DIR}"
 
 rmdir "${TMP}"
 
