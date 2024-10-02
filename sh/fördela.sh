13c0ee38f382668ba3ac99b2c8fe5f7866d3d095
Johan Öhlin <johanohlin@gmx.com>
2024-10-02 19:56:52 +0200

    given argument, make it evenly distributed in mpd

---
+++ sh/fördela.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+mpc -qw update
+
+artist="(.+)"
+title="(.+)"
+album="(.*)"
+time="(.+)"
+freq="(.*)"
+
+if [ $# -eq 0 ]; then
+        >&2 echo "Arguments must exist"
+        exit 1
+fi
+
+# Arguments
+while getopts "a:t:A:T:f:" option; do
+        case "${option}"
+                in
+                a) artist=${OPTARG//[$?!]/\.};;
+                A) album=${OPTARG//[$?!]/\.};;
+                f) freq=${OPTARG//[$?!]/\.};;
+                t) title=${OPTARG//[$?!]/\.};;
+                T) time=${OPTARG//[$?!]/\.};;
+                *) >&2 echo "faulty flag: ${option}"; exit 1;;
+        esac
+done
+
+# Formats
+mpc_format="%artist% - %title% (%album%) %time%"
+grep_format="$artist - $title \($album\) $time"
+
+
+playlist_length=$(mpc playlist | wc -l)
+current_position=$(mpc -f "%position%" current)
+
+occurance=$(mpc -f "$mpc_format" playlist | \
+        tail -n "$((playlist_length-current_position))" | \
+        grep -Ec '^'"${grep_format}"'$')
+
+
+if [ "$occurance" -eq 0 ]; then
+        >&2 echo "No occurances of \"$grep_format\" found."
+        exit 1
+fi
+
+c=${current_position}
+diff=$((playlist_length-c))
+
+# frequency
+if [ -z ${freq+x} ]; then 
+        f=${freq}
+        if [ "$f" -le 1 ]; then
+                >&2 echo "argument -f ($f) has to be greater than 1" >&2 
+                exit 2
+        fi
+else
+        f=$(echo "$diff / $occurance" | bc -l )
+fi
+
+last_song="$(mpc -f "%file%" playlist | tail -1)"
+# Move all matching to the end
+mpc -f "%position% ${mpc_format}" playlist  | \
+        tail -n ${diff} | \
+        grep -E '^'"[0-9]+ ${grep_format}"'$'    | \
+        cut -d" " -f1   | \
+        tac             | \
+        while read -r pos; do
+                mpc mv "$pos" "$playlist_length"
+                echo "$pos -> $playlist_length"
+        done
+
+# Move up last songs
+i=1
+n=$(echo "$c + $i * $f" | bc | cut -d'.' -f1)
+while [ "$(mpc -f "%file%" playlist | tail -1)" != "${last_song}" ]; do
+        if [ "$n" -gt "$playlist_length" ]; then
+                break
+        else
+                pos=${playlist_length}
+                mpc -q mv "$pos" "$n"
+                echo "$pos -> $n"
+                # Next position
+                ((i++))
+                n=$(echo "$c + $i * $f" | bc | cut -d'.' -f1)
+        fi
+done
+
