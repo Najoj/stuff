621abd191baa55a689a67b743bc84687bb00286f
Johan Öhlin <johanohlin@gmx.com>
2024-10-02 19:53:13 +0200

    extra checks

--- sh/rmpl.sh
+++ sh/rmpl.sh
@@ -2,18 +2,20 @@
 
 SPELA_KLART="/home/jojan/src/spela_klart"
 ROOT="/media/musik"
+cd "$ROOT" || exit 1
 
 _delete_file() {
         # Deletes file from hard drive
         echo -n "rm ${1}... "
         "$SPELA_KLART"
-        flock -x "$MPC_LOCK" -c "rm -f -- \"$1\""
+        flock -x "$MPC_LOCK" -c "rm -f -- \"$1\" && mpc -wq update"
         echo "done!"
 }
 _remove_file() {
         # Removes file from playlist
         echo -n "mpc del ${1}... "
-        flock -x "$MPC_LOCK" -c "$SPELA_KLART && mpc del \"$1\""
+        san1="$(echo "$1" | tr '[:punct:]' '.')"
+        flock -x "$MPC_LOCK" -c "$SPELA_KLART && mpc -f \"%position% %file%\" playlist | grep -E \" ${san1}$\" | cut -d\" \" -f1 | xargs mpc del"
         echo "done!"
 }
 
@@ -41,17 +43,14 @@ if [ "$1" = "-f" ] || [ "$1" = "-i" ]; then
 fi
 
 # file to remove
-file_name="$ROOT/$(mpc -f "%file%" current)"
+file_name="$(mpc -f "%file%" current)"
 ogg_file=${file_name%ogg}ogg
-# file position in list
-file_pos=$(mpc -f "%position%" current)
 
-mpc -f "%artist% - %title%" current
 if $inverted; then
         if [ -e "${file_name}" ]; then
-                _remove_file "$file_pos"
+                _remove_file "$file_name"
         else
-                _delete_file "$file_name"
+                _delete_file "$ogg_file"
 
         fi
 else
@@ -59,7 +58,7 @@ else
         if [ -e "${ogg_file}" ]; then
                 _delete_file "$file_name"
         else
-                _remove_file "$file_pos"
+                _remove_file "$file_name"
         fi
 fi
 
