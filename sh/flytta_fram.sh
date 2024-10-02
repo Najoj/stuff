7be89ebfa96838b767e3cc121718416b269be59d
Johan Öhlin <johanohlin@gmx.com>
2024-10-02 19:56:16 +0200

    given artist, move all song ahead in mpd

---
+++ sh/flytta_fram.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Option to ensure success is changed saved from loop
+shopt -s lastpipe
+
+mpc -wq update
+
+if [ "$1" ]; then
+        what="%$1%"
+else
+        what="%artist%"
+fi
+
+if [ "$2" ]; then
+        match="$2"
+else
+        match="$(mpc -f "$what" current)"
+fi
+match="$(echo "${match}" | tr '!?'"$'\(\)" '.')"
+
+c=$(mpc -f "%position%" current)
+((j=$((RANDOM % 7 + 5))))
+((i=j))
+
+success=false
+mpc -f "%position% $what" playlist | \
+        grep --color=never -E "^[1-9]([0-9]*) ${match}$" | \
+        cut -d" " -f1 | while read -r pos; do
+        if [ "$pos" -gt "$((c+j))" ]; then
+                ((n=c+i))
+                ((i+=j))
+                printf "%'d -> %'d\n" "$pos" "$n"
+                mpc -q mv "$pos" "$n"
+                success=true
+        fi
+done
+
+if $success; then
+        exit 0
+fi
+exit 1
+
