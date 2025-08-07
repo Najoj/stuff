b42f657739ecdc8befcb50f286121c21ed468a88
Johan Öhlin <johanohlin@gmx.com>
2025-08-07 12:19:14 +0200

    move settings to youtube-dl config

--- sh/youtube.sh
+++ sh/youtube.sh
@@ -3,14 +3,8 @@ source "${HOME}/src/utils.sh"
 
 echo "$@" | grep -E "/(channel|user)/" && exit 1
 
-COOKIES="${HOME}/.ytdlc"
-SETTINGS="${HOME}/.yts"
-
 required_programs youtube-dl sleep || exit 1
 required_files "$COOKIES" "$SETTINGS" || exit 1
-
-USERNAME="$(head -1 "$SETTINGS")"
-PASSWORD="$(tail -1 "$SETTINGS")"
 
 SLEEP="${HOME}/src/sleep"
 [ -f "$SLEEP" ] || SLEEP="sleep"
@@ -19,14 +13,9 @@ WAIT="5s"
 ((LIM=5))
 ((C=0))
 
+# Most setting are controlled from youtube-dl's config file
 until youtube-dl \
-        --username="$USERNAME" \
-        --password="$PASSWORD" \
-        --cookies="$COOKIES" \
         --ignore-errors \
-        --no-playlist \
-        --continue \
-        --format="bestvideo[height<=?1028]+bestaudio/best" \
         "${@: 1:((${#}-1))}" -- "${@: -1}" || [ "$C" -gt "$LIM" ] ; do
 	${SLEEP} "${WAIT}"
     ((C++))
